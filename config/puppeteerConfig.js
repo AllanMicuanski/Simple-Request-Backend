7f0a4dbcff71c9ecd646831ed2a89c74937e3c60
AllanMicuanski <allanmicuanski@gmail.com>
2024-10-17 15:08:07 -0300

    fix:package

--- config/puppeteerConfig.js
+++ config/puppeteerConfig.js
@@ -1,4 +1,6 @@
 module.exports = {
+  executablePath:
+    "/opt/render/.cache/puppeteer/chrome/linux-130.0.6723.58/chrome-linux64/chrome",
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
   headless: true,
 };
