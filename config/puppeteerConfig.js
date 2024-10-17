d5352805f950bb508759eb62e4fd5f895b13a7ad
AllanMicuanski <allanmicuanski@gmail.com>
2024-10-17 10:48:44 -0300

    fix:config puppeter

--- config/puppeteerConfig.js
+++ config/puppeteerConfig.js
@@ -1,4 +1,7 @@
+const puppeteer = require("puppeteer");
+
 module.exports = {
   args: ["--no-sandbox", "--disable-setuid-sandbox"],
   headless: true,
+  executablePath: puppeteer.executablePath(),
 };
